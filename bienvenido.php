d613b9bdbb3208c77f567fa85145cb209050bf83
FelipeUrrea <82669336+FelipeUrrea@users.noreply.github.com>
2023-03-01 14:46:27 -0500

    primer commit

---
+++ bienvenido.php
@@ -0,0 +1,33 @@
+<?php
+    
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        echo '
+        <script>
+            alert("Por favor debes iniciar sesión");
+            window.location = "index.php";
+        </script>
+        ';
+        header("location: index.php");
+        session_destroy();
+        die();
+
+    }
+    session_destroy();
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bienvenido - USB</title>
+</head>
+<body>
+    <h1> Bienvendo a mi pagina </h1>
+    <a href="php/cerrar_sesion.php">Cerrar sesión</a>
+</body>
+</html>
